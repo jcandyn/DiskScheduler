f00c495e3b91be91183c61afe3ac9aed1778b9c5
Joscandy <jos.candy@gmail.com>
2022-11-11 16:19:42 -0500

    added validation for requests and put sorting in a method

--- scheduler.c
+++ scheduler.c
@@ -8,6 +8,25 @@ int end = 4999;
 
 int numberOfElemsInArray(int arr[])
 {
+    return 0;
+}
+
+int *sortArray(int disc_req[], int SIZE)
+{
+    int temp = 0;
+    for (int i = 0; i < SIZE + 1; i++)
+    {
+        for (int j = i + 1; j < SIZE + 1; j++)
+        {
+            if (disc_req[i] > disc_req[j])
+            {
+                temp = disc_req[i];
+                disc_req[i] = disc_req[j];
+                disc_req[j] = temp;
+            }
+        }
+    }
+    return disc_req;
 }
 
 void fcfs(int disc_req[], int head, int SIZE)
@@ -45,7 +64,9 @@ void clook(int disc_req[], int head, int SIZE)
 {
     int totalHeadMoves = 0;
     // create new array with one more space for an element
-    int newArr[SIZE + 1];
+    int arr[SIZE + 1];
+    int *newArr = arr;
+
     for (int i = 0; i < SIZE; i++)
     {
         newArr[i] = disc_req[i]; // populate array with elements of our old array
@@ -55,19 +76,7 @@ void clook(int disc_req[], int head, int SIZE)
     newArr[SIZE] = head;
 
     // first sort array
-    int temp = 0;
-    for (int i = 0; i < SIZE + 1; i++)
-    {
-        for (int j = i + 1; j < SIZE + 1; j++)
-        {
-            if (newArr[i] > newArr[j])
-            {
-                temp = newArr[i];
-                newArr[i] = newArr[j];
-                newArr[j] = temp;
-            }
-        }
-    }
+    newArr = sortArray(newArr, SIZE);
 
     int index = -1;
 
@@ -120,7 +129,9 @@ void CSCAN(int disc_req[], int head, int length)
     int totalHeadMoves = 0;
 
     // create new array with 3 more spaces for head, end of track and beginning (4999 and 0 respectively)
-    int newArr[length + 3];
+    int arr[length + 3];
+    int *newArr = arr;
+
     for (int i = 0; i < length; i++)
     {
         newArr[i] = disc_req[i]; // populate array with elements of our old array
@@ -133,19 +144,7 @@ void CSCAN(int disc_req[], int head, int length)
     int newLength = length + 3;
 
     // first sort array
-    int temp = 0;
-    for (int i = 0; i < newLength + 1; i++)
-    {
-        for (int j = i + 1; j < newLength + 1; j++)
-        {
-            if (newArr[i] > newArr[j])
-            {
-                temp = newArr[i];
-                newArr[i] = newArr[j];
-                newArr[j] = temp;
-            }
-        }
-    }
+    newArr = sortArray(newArr, newLength);
 
     int index = -1;
 
@@ -197,7 +196,8 @@ void SCAN(int disc_req[], int head, int length)
     int totalHeadMoves = 0;
 
     // create new array with 3 more spaces for head, end of track and beginning (4999 and 0 respectively)
-    int newArr[length + 2];
+    int arr[length + 2];
+    int *newArr = arr;
     for (int i = 0; i < length; i++)
     {
         newArr[i] = disc_req[i]; // populate array with elements of our old array
@@ -209,19 +209,7 @@ void SCAN(int disc_req[], int head, int length)
     int newLength = length + 2;
 
     // first sort array
-    int temp = 0;
-    for (int i = 0; i < newLength + 1; i++)
-    {
-        for (int j = i + 1; j < newLength + 1; j++)
-        {
-            if (newArr[i] > newArr[j])
-            {
-                temp = newArr[i];
-                newArr[i] = newArr[j];
-                newArr[j] = temp;
-            }
-        }
-    }
+    newArr = sortArray(newArr, newLength);
 
     int index = -1;
 
@@ -267,7 +255,7 @@ void SCAN(int disc_req[], int head, int length)
     printf("\nTotal head movements = %d \n", totalHeadMoves);
 }
 
-void look(int disc_req[], int head, int SIZE)
+void look(int *disc_req, int head, int SIZE)
 {
     int totalHeadMoves = 0;
     // create new array with one more space for an element
@@ -281,20 +269,7 @@ void look(int disc_req[], int head, int SIZE)
     newArr[SIZE] = head;
 
     // first sort array
-    int temp = 0;
-    for (int i = 0; i < SIZE + 1; i++)
-    {
-        for (int j = i + 1; j < SIZE + 1; j++)
-        {
-            if (newArr[i] > newArr[j])
-            {
-                temp = newArr[i];
-                newArr[i] = newArr[j];
-                newArr[j] = temp;
-            }
-        }
-    }
-
+    disc_req = sortArray(disc_req, SIZE);
     int index = -1;
 
     // find index of head
@@ -340,24 +315,42 @@ void look(int disc_req[], int head, int SIZE)
     printf("\nTotal head movements = %d \n", totalHeadMoves);
 }
 
+int validateRequests(int disc_req[], int SIZE)
+{
+    // first sort array
+    disc_req = sortArray(disc_req, SIZE);
+
+    // check if element at first index is less than 0
+    if (disc_req[0] < 0 || disc_req[SIZE - 1] > 4999)
+    {
+        return 0;
+    }
+    return 1;
+}
+
 // Driver code
 int main()
 {
     // request array
-
     // int disc_req[10] = {2069, 1212, 2296, 2800, 544, 1618, 356, 1523, 4965, 3681}; // sample request
-
     int disc_req[8] = {176, 79, 34, 60, 92, 11, 41, 114}; // sample request
-    int head;                                             // this will be obtained from command line input
     int length = sizeof(disc_req) / sizeof(disc_req[0]);
 
+    int isValid = validateRequests(disc_req, length);
+    if (isValid == 0)
+    {
+        printf("Range of values: 0 to 4999. Entered values are not within range.");
+        exit(0);
+    }
+
+    int head;
     printf("Enter an initial head value : ");
     scanf("%d", &head);
 
     // fcfs(disc_req, head, length);
-    // clook(disc_req, head, length);
+    clook(disc_req, head, length);
     // CSCAN(disc_req, head, length);
     // SCAN(disc_req, head, length);
-    look(disc_req, head, length);
+    // look(disc_req, head, length);
     return 0;
 }
