04ec85d2f030c2e7921f083ea99e61a10a2059f0
dascolt1 <dascolt1@tcnj.edu>
2022-11-10 19:59:25 -0500

    cscan algo done

--- scheduler.c
+++ scheduler.c
@@ -5,6 +5,11 @@
 int track, head, distance;
 int SIZE = 8;
 int start = 0;
+int end = 4999;
+
+int numberOfElemsInArray(int arr[]) {
+
+}
 
 void fcfs(int disc_req[], int head)
 {
@@ -110,6 +115,85 @@ void clook(int disc_req[], int head)
     printf("\nTotal head movements = %d \n", totalHeadMoves);
 }
 
+ void CSCAN(int disc_req[], int head, int length)
+{
+    
+    int totalHeadMoves = 0;
+  
+    // create new array with 3 more spaces for head, end of track and beginning (4999 and 0 respectively)
+    int newArr[length + 3];
+    for (int i = 0; i < length; i++)
+    {
+        newArr[i] = disc_req[i]; // populate array with elements of our old array
+    }
+
+    // add the head to this new array
+    newArr[length] = head;
+    newArr[length + 1] = end;
+    newArr[length + 2] = start;
+    int newLength = length + 3;
+   
+
+    // first sort array
+    int temp = 0;
+    for (int i = 0; i < newLength + 1; i++)
+    {
+        for (int j = i + 1; j < newLength + 1; j++)
+        {
+            if (newArr[i] > newArr[j])
+            {
+                temp = newArr[i];
+                newArr[i] = newArr[j]; 
+                newArr[j] = temp;
+            }
+        }
+    }
+
+
+    int index = -1;
+
+    // find index of head
+    for (int i = 0; i < newLength + 1; i++)
+    {
+        if (newArr[i] == head)
+        {
+            index = i;
+            break;
+        }
+    }
+    
+    printf("%s", "Seek Sequence is: \n");
+    for (int i = index + 1; i < newLength + 1; i++)
+    {
+        track = newArr[i];
+        // get distance
+        distance = abs(track - head);
+
+        // increase the total head moves count
+        totalHeadMoves += distance;
+
+        // the current track becomes the new head
+        head = track;
+        printf("%d ", newArr[i]); // print all elements starting from head
+    }
+    
+    for (int i = 1; i < index; i++)
+    {
+        track = newArr[i];
+        // get distance
+        distance = abs(track - head);
+
+        // increase the total head moves count
+        totalHeadMoves += distance;
+
+        // the current track becomes the new head
+        head = track;
+        printf("%d ", newArr[i]); // print all elements starting from lowest value until head
+    }
+
+    printf("\nTotal head movements = %d \n", totalHeadMoves);
+}
+
 // Driver code
 int main()
 {
@@ -119,9 +203,11 @@ int main()
 
     int disc_req[8] = {176, 79, 34, 60, 92, 11, 41, 114}; // sample request
     int head = 50;                                        // this will be obtained from command line input
+    int length = sizeof(disc_req) / sizeof(disc_req[0]);
 
     // fcfs(disc_req, head);
-    clook(disc_req, head);
+    //clook(disc_req, head);
+    CSCAN(disc_req, head, length);
 
     return 0;
 }
